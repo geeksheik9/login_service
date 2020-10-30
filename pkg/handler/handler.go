c456a4f899c34a4cbdbfb9331ce54c494921b9b8
Ben Abrams <Benjamin.Abrams@Centene.com>
2020-10-30 14:48:08 -0400

    added basic user model, added db setup for login

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,12 +1,24 @@
 package handler
 
 import (
+	"github.com/geeksheik9/login-service/models"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+//LoginDatabase is the interface setup for the login service
+type LoginDatabase interface {
+	CreateUser(models.User) error
+	LoginUser(models.User) error
+	UpdateUser(models.User) error
+	FindUser(mongoID primitive.ObjectID) models.User
+	DeleteUser(mongoID primitive.ObjectID) error
+}
 
 //LoginService is the implementation of a service to login to an application
 type LoginService struct {
-	Version string
+	Version  string
+	Database LoginDatabase
 }
 
 //Routes sets up the routes for the RESTful interface
